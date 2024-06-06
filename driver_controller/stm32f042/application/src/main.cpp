83d116bb541264bd3da75d35b363b9c697d54276
Carlo <cpeniche777@hotmail.com>
2024-06-06 00:05:38 -0700

    Free Rtos

--- driver_controller/stm32f042/application/src/main.cpp
+++ driver_controller/stm32f042/application/src/main.cpp
@@ -22,6 +22,8 @@
 #include "scheduler.h"
 #include "stm32f0xx_hal_can.h"
 #include "motor.h"
+#include "FreeRTOS.h"
+#include "task.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -53,6 +55,9 @@ extern "C" {
 
 UART_HandleTypeDef huart2;
 TIM_HandleTypeDef htim3;
+
+
+#define mainQUEUE_RECEIVE_TASK_PRIORITY     ( tskIDLE_PRIORITY + 2 )
 
 /* USER CODE BEGIN PV */
 
@@ -243,7 +248,7 @@ static void Spi_task()
 }
 
 
-static void Can_task()
+static void Can_task(void *pvParameters)
 {
   static MCU* mcu = MCU::get_mcu_instance();
   Can_Rx_Msg<CAN_RxHeaderTypeDef> rx_msg;
@@ -302,7 +307,17 @@ int main()
 	mcu->can->driver.Init();
   MX_TIM3_Init();
 
+  xTaskCreate(Can_task,                   /* The function that implements the task. */
+              "Can Recevie Task",                                   /* The text name assigned to the task - for debug only as it is not used by the kernel. */
+              4 * configMINIMAL_STACK_SIZE,           /* The size of the stack to allocate to the task. */
+              NULL, /* The parameter passed to the task - just to check the functionality. */
+              mainQUEUE_RECEIVE_TASK_PRIORITY,        /* The priority assigned to the task. */
+              NULL );       
+  /* Start the tasks and timer running. */
+  vTaskStartScheduler();
+  while(1);
 
+#if 0
 	Scheduler *sched = new Scheduler();
 	//Task *spi_task=new Task(&Spi_task,1000,20);
 	Task *can_task=new Task(&Can_task,10,0);
@@ -310,13 +325,90 @@ int main()
 	//sched->add_task(*spi_task);
 	sched->add_task(*can_task);
   sched->add_task(*motor_task);
- 
 
 	do
 	{
 	  sched->exec();
 	}while(1);
+#endif
+
+
 }
+
+
+/*-----------------------------------------------------------*/
+
+void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName )
+{
+    /* If configCHECK_FOR_STACK_OVERFLOW is set to either 1 or 2 then this
+     * function will automatically get called if a task overflows its stack. */
+    ( void ) pxTask;
+    ( void ) pcTaskName;
+    for( ;; );
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationMallocFailedHook( void )
+{
+    /* If configUSE_MALLOC_FAILED_HOOK is set to 1 then this function will
+     * be called automatically if a call to pvPortMalloc() fails.  pvPortMalloc()
+     * is called automatically when a task, queue or semaphore is created. */
+    for( ;; );
+}
+
+/*-----------------------------------------------------------*/
+
+/* configUSE_STATIC_ALLOCATION is set to 1, so the application must provide an
+ * implementation of vApplicationGetIdleTaskMemory() to provide the memory that is
+ * used by the Idle task. */
+extern void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize )
+{
+/* If the buffers to be provided to the Idle task are declared inside this
+ * function then they must be declared static - otherwise they will be allocated on
+ * the stack and so not exists after this function exits. */
+static StaticTask_t xIdleTaskTCB;
+static StackType_t uxIdleTaskStack[ configMINIMAL_STACK_SIZE ];
+
+    /* Pass out a pointer to the StaticTask_t structure in which the Idle task's
+     * state will be stored. */
+    *ppxIdleTaskTCBBuffer = &xIdleTaskTCB;
+
+    /* Pass out the array that will be used as the Idle task's stack. */
+    *ppxIdleTaskStackBuffer = uxIdleTaskStack;
+
+    /* Pass out the size of the array pointed to by *ppxIdleTaskStackBuffer.
+     * Note that, as the array is necessarily of type StackType_t,
+     * configMINIMAL_STACK_SIZE is specified in words, not bytes. */
+    *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
+}
+
+/*-----------------------------------------------------------*/
+
+/* configUSE_STATIC_ALLOCATION and configUSE_TIMERS are both set to 1, so the
+ * application must provide an implementation of vApplicationGetTimerTaskMemory()
+ * to provide the memory that is used by the Timer service task. */
+extern void vApplicationGetTimerTaskMemory( StaticTask_t **ppxTimerTaskTCBBuffer, StackType_t **ppxTimerTaskStackBuffer, uint32_t *pulTimerTaskStackSize )
+{
+/* If the buffers to be provided to the Timer task are declared inside this
+ * function then they must be declared static - otherwise they will be
+ * allocated on the stack and so not exists after this function exits. */
+static StaticTask_t xTimerTaskTCB;
+static StackType_t uxTimerTaskStack[ configTIMER_TASK_STACK_DEPTH ];
+
+    /* Pass out a pointer to the StaticTask_t structure in which the Timer
+     * task's state will be stored. */
+    *ppxTimerTaskTCBBuffer = &xTimerTaskTCB;
+
+    /* Pass out the array that will be used as the Timer task's stack. */
+    *ppxTimerTaskStackBuffer = uxTimerTaskStack;
+
+    /* Pass out the size of the array pointed to by *ppxTimerTaskStackBuffer.
+     * Note that, as the array is necessarily of type StackType_t,
+     * configMINIMAL_STACK_SIZE is specified in words, not bytes. */
+    *pulTimerTaskStackSize = configTIMER_TASK_STACK_DEPTH;
+}
+/*-----------------------------------------------------------*/
+
 
 
 extern "C" void CEC_CAN_IRQHandler()
