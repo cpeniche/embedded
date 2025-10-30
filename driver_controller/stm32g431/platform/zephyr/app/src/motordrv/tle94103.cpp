8988356c178c25854c8313bf971f947db98387ed
cpeniche <cpeniche777@hotmail.com>
2025-10-29 20:10:24 -0700

    Add files for motor control drivers

--- driver_controller/stm32g431/platform/zephyr/app/src/motordrv/tle94103.cpp
+++ driver_controller/stm32g431/platform/zephyr/app/src/motordrv/tle94103.cpp
@@ -3,8 +3,32 @@ LOG_MODULE_REGISTER(tle94103, LOG_LEVEL_DBG);
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/spi.h>
 #include "spiBuilder.h"
+#include "motorctrl.h"
 #include "tle94103.h"
 
+void Tle94103::Right(void)
+{
+  uint8_t data = HB1HSEN | HB3LSEN;
+  WriteRegister(HBCTRL, &data);
+}
+
+void Tle94103::Left(void)
+{
+  uint8_t data = HB1LSEN | HB3HSEN;
+  WriteRegister(HBCTRL, &data);
+}
+
+void Tle94103::Down(void)
+{
+  uint8_t data = HB2HSEN | HB3LSEN;
+  WriteRegister(HBCTRL, &data);
+}
+
+void Tle94103::Up(void)
+{
+  uint8_t data = HB3HSEN | HB2LSEN;
+  WriteRegister(HBCTRL, &data);
+}
 
 Tle94103::Tle94103(spiInterface<uint8_t, int16_t> *spi)
 {
@@ -14,10 +38,9 @@ Tle94103::Tle94103(spiInterface<uint8_t, int16_t> *spi)
 void Tle94103::WriteRegister(uint8_t u8RegAddress, uint8_t *ptrU8Data)
 {
 
-  uint8_t u8Buffer[]={uint8_t(W_R(1) | 
-                      ADDRESS(u8RegAddress) | 
-                      LABT(1) | 0x1),
-                      *ptrU8Data};
-  spi->Write(u8Buffer,2);
-
+  uint8_t u8Buffer[] = {uint8_t(W_R(1) |
+                                ADDRESS(u8RegAddress) |
+                                LABT(1) | 0x1),
+                        *ptrU8Data};
+  spi->Write(u8Buffer, 2);
 }
