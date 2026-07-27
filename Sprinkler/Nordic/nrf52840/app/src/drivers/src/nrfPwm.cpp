3f5b7506a77640faa16d2b0c6839ab5814ad079a
cpeniche <cpeniche777@hotmail.com>
2026-07-26 19:14:03 -0700

    Add pwm to the characteristics and compile with the pwm library

--- Sprinkler/Nordic/nrf52840/app/src/drivers/src/nrfPwm.cpp
+++ Sprinkler/Nordic/nrf52840/app/src/drivers/src/nrfPwm.cpp
@@ -2,28 +2,28 @@
 #include <zephyr/sys/printk.h>
 #include "nrfPwm.h"
 
-nrfPwm* nrfPwm::_instance=nullptr;
+nrfPwm *nrfPwm::_instance = nullptr;
 
 nrfPwm *nrfPwm::getInstance(void)
 {
   if (_instance == nullptr)
-  {   
-    _instance = new nrfPwm();    
-  }    
+  {
+    _instance = new nrfPwm();
+  }
   return _instance;
 }
 
 int8_t nrfPwm::setDutyCycle(uint8_t dutyCycle)
 {
-  uint32_t pulse=_valve.period/100*dutyCycle;
-  _dutyCycle=dutyCycle;
-  return pwm_set_dt(&_valve, _valve.period,pulse);
+  uint32_t pulse = _valve.period / 100 * dutyCycle;
+  _dutyCycle = dutyCycle;
+  return pwm_set_dt(&_valve, _valve.period, pulse);
 }
 
 int8_t nrfPwm::setFrequency(uint32_t period)
 {
-  
-  uint32_t pulse=period/100*_dutyCycle;
+
+  uint32_t pulse = period / 100 * _dutyCycle;
   return pwm_set_dt(&_valve, period, pulse);
 }
 
@@ -39,8 +39,8 @@ int8_t nrfPwm::stop()
 
 nrfPwm::nrfPwm(void)
 {
-    
-  if(pwm_is_ready_dt(&_valve))
+
+  if (pwm_is_ready_dt(&_valve))
   {
     pwm_set_dt(&_valve, _valve.period, 0);
   }
@@ -50,7 +50,13 @@ nrfPwm::nrfPwm(void)
   }
 }
 
-extern "C" void CreatePwmInstance(void)
+extern "C" void DrivePwm(uint8_t index, uint8_t value)
 {
-  nrfPwm::getInstance();
+
+  nrfPwm *pwm = nrfPwm::getInstance();
+
+  printk("index = %u, value=%u\n", index, value);
+
+  if (index == 0)
+    pwm->setDutyCycle(value);
 }
